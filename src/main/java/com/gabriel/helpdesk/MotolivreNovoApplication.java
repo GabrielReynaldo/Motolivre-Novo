671b775b159877b9092c660e61273555112f6050
GabrielReynaldo <gabrielreynaldo123@gmail.com>
2022-07-15 09:21:31 -0300

    projeto iniciado

---
+++ src/main/java/com/gabriel/helpdesk/MotolivreNovoApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotolivreNovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotolivreNovoApplication.class, args);
+	}
+
+}
