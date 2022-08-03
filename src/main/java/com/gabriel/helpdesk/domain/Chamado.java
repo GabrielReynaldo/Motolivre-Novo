a580ac6866aaed6ed139b819cdb22d90a0d1faeb
GabrielReynaldo <gabrielreynaldo123@gmail.com>
2022-08-03 20:25:37 -0300

    Anotações jpa para classes de dominio

--- src/main/java/com/gabriel/helpdesk/domain/Chamado.java
+++ src/main/java/com/gabriel/helpdesk/domain/Chamado.java
@@ -3,20 +3,41 @@ package com.gabriel.helpdesk.domain;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.gabriel.helpdesk.domain.enums.Prioridade;
 import com.gabriel.helpdesk.domain.enums.Status;
 
+@Entity
 public class Chamado {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy" )
 	private LocalDate dataAbertura = LocalDate.now();
+	@JsonFormat(pattern = "dd/MM/yyyy" )
 	private LocalDate dataFechamento;
+	
+	
 	private Prioridade prioridade;
 	private Status status;
 	private String titulo;
 	private String observacoes;
 	
+	@ManyToOne
+	@JoinColumn(name = "estabelecimento_id")
 	private Estabelecimento estabelecimento;
+	
+	@ManyToOne
+	@JoinColumn(name = "motoboy_id")
 	private Motoboy motoboy;
 	public Chamado() {
 		super();
--- src/main/java/com/gabriel/helpdesk/domain/Estabelecimento.java
+++ src/main/java/com/gabriel/helpdesk/domain/Estabelecimento.java
@@ -3,8 +3,14 @@ package com.gabriel.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Estabelecimento extends Pessoa{
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
+@Entity
+public class Estabelecimento extends Pessoa{
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "estabelecimento")
 	private List<Chamado> chamados = new ArrayList<>();
 
 	public Estabelecimento() {
