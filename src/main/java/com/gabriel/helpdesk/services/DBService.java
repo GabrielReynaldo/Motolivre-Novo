9a024e57cb539bb30606c5db7d268616994f11db
GabrielReynaldo <gabrielreynaldo123@gmail.com>
2022-08-08 00:48:18 -0300

    autenticacao com jwt

--- src/main/java/com/gabriel/helpdesk/services/DBService.java
+++ src/main/java/com/gabriel/helpdesk/services/DBService.java
@@ -3,6 +3,7 @@ package com.gabriel.helpdesk.services;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.helpdesk.domain.Chamado;
@@ -12,29 +13,28 @@ import com.gabriel.helpdesk.domain.enums.Perfil;
 import com.gabriel.helpdesk.domain.enums.Prioridade;
 import com.gabriel.helpdesk.domain.enums.Status;
 import com.gabriel.helpdesk.repositories.ChamadoRepository;
-import com.gabriel.helpdesk.repositories.EstabelecimentoRepository;
-import com.gabriel.helpdesk.repositories.MotoboyRepository;
+import com.gabriel.helpdesk.repositories.PessoaRepository;
 
 @Service
 public class DBService {
 	
 	@Autowired
-	private EstabelecimentoRepository estabelecimentoRepository;
-	@Autowired
-	private MotoboyRepository motoboyRepository;
-	@Autowired
 	private ChamadoRepository chamadoRepository;
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	@Autowired
+	private BCryptPasswordEncoder encoder;
+	
 	
 	public void instanciaDB() {
-		Estabelecimento est1 = new Estabelecimento(null, "Gabriel 1","15975708001","gabriel1@gmail.com","123");
+		Estabelecimento est1 = new Estabelecimento(null, "Gabriel 1","15975708001","gabriel1@gmail.com",encoder.encode("123"));
 		est1.addPerfil(Perfil.ADMIN);
 		
-		Motoboy mot1 = new Motoboy(null, "Gabriel 2","41995562068","gabriel2@gmail.com","123"); 
+		Motoboy mot1 = new Motoboy(null, "Gabriel 2","41995562068","gabriel2@gmail.com",encoder.encode("123")); 
 		
 		Chamado c1 = new Chamado(null,Prioridade.MEDIA,Status.ANDAMENTO,"Chamado 01","Primeiro chamado",est1,mot1);
 		
-		estabelecimentoRepository.saveAll(Arrays.asList(est1));
-		motoboyRepository.saveAll(Arrays.asList(mot1));
+		pessoaRepository.saveAll(Arrays.asList(est1, mot1));
 		chamadoRepository.saveAll(Arrays.asList(c1));
 	}
 
