47821a8a8b2e7e70f6498b5d9e1b58ba3dd5683c
GabrielReynaldo <gabrielreynaldo123@gmail.com>
2022-08-21 21:46:42 -0300

    salvando alterações

--- src/main/java/com/gabriel/helpdesk/services/DBService.java
+++ src/main/java/com/gabriel/helpdesk/services/DBService.java
@@ -29,13 +29,19 @@ public class DBService {
 	public void instanciaDB() {
 		Estabelecimento est1 = new Estabelecimento(null, "Gabriel 1","15975708001","gabriel1@gmail.com",encoder.encode("123"));
 		est1.addPerfil(Perfil.ADMIN);
+		Estabelecimento est2 = new Estabelecimento(null, "Gabriel 1","47549328030","gabriel4@gmail.com",encoder.encode("123"));
+		est2.addPerfil(Perfil.ADMIN);
 		
-		Motoboy mot1 = new Motoboy(null, "Gabriel 2","41995562068","gabriel2@gmail.com",encoder.encode("123")); 
-		
+		Motoboy mot1 = new Motoboy(null, "Gabriel 2","41995562068","gabriel2@gmail.com",encoder.encode("123"));
+		mot1.addPerfil(Perfil.ADMIN);
+		Motoboy mot2 = new Motoboy(null, "Gabriel 2","15382201056","gabriel3@gmail.com",encoder.encode("123")); 
+		mot2.addPerfil(Perfil.ADMIN);
 		Chamado c1 = new Chamado(null,Prioridade.MEDIA,Status.ANDAMENTO,"Chamado 01","Primeiro chamado",est1,mot1);
 		
-		pessoaRepository.saveAll(Arrays.asList(est1, mot1));
+		pessoaRepository.saveAll(Arrays.asList(est1,est2 , mot1, mot2));
 		chamadoRepository.saveAll(Arrays.asList(c1));
 	}
+	
+	
 
 }
